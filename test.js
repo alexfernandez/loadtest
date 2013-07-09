75e6bed768c7d49f1f978f9a145f84d83c0a77db
Alex Fernández <alex.fernandez@mediasmart.es>
2013-07-09 19:43:10 +0200

    Changed tests to use the testing library.

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ var prototypes = require('./lib/prototypes.js');
 var timing = require('./lib/timing.js');
 var sample = require('./lib/sample.js');
 var util = require('util');
-var async = require('async');
+var testing = require('testing');
 var Log = require('log');
 
 // globals
@@ -22,39 +22,17 @@ var log = new Log('info');
  */
 exports.test = function(callback)
 {
-	var run = false;
 	var tests = {
 		prototypes: prototypes.test,
 		timing: timing.test,
 		sample: sample.test,
 	};
-	async.series(tests, function(error, result)
-	{
-		run = true;
-		callback(error, result);
-	});
-	// give it time
-	setTimeout(function()
-	{
-		if (!run)
-		{
-			callback('Package tests did not call back');
-		}
-	}, 2200);
+	testing.run(tests, 2200, callback);
 }
 
 // run tests if invoked directly
 if (__filename == process.argv[1])
 {
-	exports.test(function(error, result)
-	{
-		if (error)
-		{
-			log.error('Failure in tests: %s', error);
-			process.exit(1);
-			return;
-		}
-		log.info('All tests successful: %s', util.inspect(result, true, 10, true));
-	});
+	exports.test(testing.show);
 }
 
