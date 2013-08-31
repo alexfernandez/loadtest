f46d49539d1fdb5cfc56f1bb76e06b94e08b52b6
zaphod1984 <zaphod84@gmx.de>
2013-08-31 12:29:34 +0200

    instrumenting optimist to parse command line parameters

--- bin/testserver.js
+++ bin/testserver.js
@@ -2,33 +2,42 @@ var testServer = require('../lib/testserver');
 
 var args = require('optimist').argv;
 
-if(args.help || args.h) {
-    console.log('Usage: testserver [options] [port]');
-    console.log('  starts a test server on the given port, default 80.');
-    console.log('Options are:');
-    console.log('    --delay           Delay the response for the given milliseconds');
-
-    process.exit(0);
-}
+if(args.help || args.h) help();
 
 var options = {};
 
-if(args._.length > 0) {
-    if(!isNaN(args._[0])) {
+if(args._.length > 0)
+{
+    if(!isNaN(args._[0]))
+	{
         options.port = parseInt(args._[0], 10);
-    } else {
-        console.log('port has to be a number');
-        process.exit(1);
+    }
+	else
+	{
+        help();
     }
 }
 
-if(args.delay) {
-    if(!isNaN(args.delay)) {
+if(args.delay)
+{
+    if(!isNaN(args.delay))
+	{
         options.delay = parseInt(args.delay);
-    } else {
-        console.log('delay has to be a number');
-        process.exit(1);
     }
+	else
+	{
+        help();
+    }
+}
+
+function help()
+{
+	console.log('Usage: testserver [options] [port]');
+	console.log('  starts a test server on the given port, default 80.');
+	console.log('Options are:');
+	console.log('    --delay           Delay the response for the given milliseconds');
+
+	process.exit(0);
 }
 
 testServer.startServer(args);
