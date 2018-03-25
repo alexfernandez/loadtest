13010239294b259ca2a31af1d05fd401c5ec758e
Alex Fernández <alexfernandeznpm@gmail.com>
2018-03-25 22:54:33 +0000

    Removed vars.

--- sample/request-generator.js
+++ sample/request-generator.js
@@ -6,9 +6,9 @@
  * https://github.com/alexfernandez/loadtest/issues/86#issuecomment-211579639
  */
 
-var loadtest = require('../lib/loadtest.js');
+const loadtest = require('../lib/loadtest.js');
 
-var options = {
+const options = {
 	url: 'http://yourHost',
 	concurrent: 5,
 	method: 'POST',
@@ -16,12 +16,12 @@ var options = {
 	requestsPerSecond:5,
 	maxSeconds:30,
 	requestGenerator: function(params, options, client, callback) {
-		var message = '{"hi": "ho"}';
+		const message = '{"hi": "ho"}';
 		options.headers['Content-Length'] = message.length;
 		options.headers['Content-Type'] = 'application/json';
 		options.body = 'YourPostData';
 		options.path = 'YourURLPath';
-		var request = client(options, callback);
+		const request = client(options, callback);
 		request.write(message);
 		return request;
 	}
