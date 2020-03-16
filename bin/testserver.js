982550474214517d95491a914a04b95a218b6d1c
Stéphane Veyret <sveyret@users.noreply.github.com>
2020-03-16 17:24:04 +0100

    Add configuration file (#174)
    Thanks to https://github.com/sveyret.

--- bin/testserver.js
+++ bin/testserver.js
@@ -9,6 +9,7 @@
 // requires
 const stdio = require('stdio');
 const testServer = require('../lib/testserver');
+const config = require('../lib/config')
 
 // init
 const options = stdio.getopt({
@@ -16,6 +17,7 @@ const options = stdio.getopt({
 	error: {key: 'e', args: 1, description: 'Return an HTTP error code'},
 	percent: {key: 'p', args: 1, description: 'Return an error (default 500) only for some % of requests'},
 });
+const configuration = config.loadConfig(options)
 if (options.args && options.args.length == 1) {
 	options.port = parseInt(options.args[0], 10);
 	if (!options.port) {
@@ -31,6 +33,16 @@ if(options.delay) {
 		process.exit(1);
 	}
 	options.delay = parseInt(options.delay, 10);
+}
+
+if(!options.delay) {
+	options.delay = configuration.delay
+}
+if(!options.error) {
+	options.error = configuration.error
+}
+if(!options.percent) {
+	options.percent = configuration.percent
 }
 
 testServer.startServer(options);
