b2b849b75656bc9aed3861e99e43812f73145fef
Deepak Shankar <shankar.deepak22@gmail.com>
2020-08-03 06:09:00 -0400

    feat:add feature to use userprovided method to replace index param

--- index.d.ts
+++ index.d.ts
@@ -21,6 +21,7 @@ declare namespace loadtest {
 		secureProtocol?: string;
 		statusCallback?(error: Error, result: any, latency: LoadTestResult): void;
 		contentInspector?(result: any): void;
+		indexParamCallBack?(): string;
 	}
 
 	export interface LoadTestResult {
