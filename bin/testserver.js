134311d242354010482b97a5f4413132f18dce6c
Alex Fernández <alexfernandeznpm@gmail.com>
2023-08-18 14:05:48 +0200

    Start test server in half the cores by default

--- bin/testserver.js
+++ bin/testserver.js
@@ -3,40 +3,63 @@
 import * as stdio from 'stdio'
 import {startServer} from '../lib/testserver.js'
 import {loadConfig} from '../lib/config.js'
+import * as cluster from 'cluster'
+import {cpus} from 'os'
+
+const options = readOptions()
+start(options)
 
 
-const options = stdio.getopt({
-	delay: {key: 'd', args: 1, description: 'Delay the response for the given milliseconds'},
-	error: {key: 'e', args: 1, description: 'Return an HTTP error code'},
-	percent: {key: 'p', args: 1, description: 'Return an error (default 500) only for some % of requests'},
-});
-const configuration = loadConfig()
-if (options.args && options.args.length == 1) {
-	options.port = parseInt(options.args[0], 10);
-	if (!options.port) {
-		console.error('Invalid port');
-		options.printHelp();
-		process.exit(1);
+function readOptions() {
+	const options = stdio.getopt({
+		delay: {key: 'd', args: 1, description: 'Delay the response for the given milliseconds'},
+		error: {key: 'e', args: 1, description: 'Return an HTTP error code'},
+		percent: {key: 'p', args: 1, description: 'Return an error (default 500) only for some % of requests'},
+		cores: {key: 'c', args: 1, description: 'Number of cores to use, default is half the total'}
+	});
+	const configuration = loadConfig()
+	if (options.args && options.args.length == 1) {
+		options.port = parseInt(options.args[0], 10);
+		if (!options.port) {
+			console.error('Invalid port');
+			options.printHelp();
+			process.exit(1);
+		}
+	}
+	if(options.delay) {
+		if(isNaN(options.delay)) {
+			console.error('Invalid delay');
+			options.printHelp();
+			process.exit(1);
+		}
+		options.delay = parseInt(options.delay, 10);
+	}
+
+	if(!options.delay) {
+		options.delay = configuration.delay
+	}
+	if(!options.error) {
+		options.error = configuration.error
+	}
+	if(!options.percent) {
+		options.percent = configuration.percent
+	}
+	if (!options.cores) {
+		// default is half the cores available
+		const totalCores = cpus().length;
+		options.cores = Math.round(totalCores / 2) || 1
+	}
+	return options
+}
+
+function start(options) {
+	if (cluster.isMaster) {
+		for (let index = 0; index < options.cores; index++) {
+			cluster.fork();
+		}
+	} else {
+		startServer(options);
 	}
 }
-if(options.delay) {
-	if(isNaN(options.delay)) {
-		console.error('Invalid delay');
-		options.printHelp();
-		process.exit(1);
-	}
-	options.delay = parseInt(options.delay, 10);
-}
 
-if(!options.delay) {
-	options.delay = configuration.delay
-}
-if(!options.error) {
-	options.error = configuration.error
-}
-if(!options.percent) {
-	options.percent = configuration.percent
-}
-
-startServer(options);
 
