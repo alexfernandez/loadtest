cf12d6518a9bdbc3c4934cd451dfd719358c9356
Alex Fernández <alexfernandeznpm@gmail.com>
2023-09-10 11:00:33 +0200

    Self-contained network performance test

---
+++ bin/network-performance.js
@@ -0,0 +1,22 @@
+import {loadTest, startServer} from '../index.js'
+
+const port = 7359;
+const serverOptions = {port}
+
+
+async function runNetworkPerformanceTest() {
+	const server = await startServer(serverOptions)
+	const options = {
+		url: `http://localhost:${port}`,
+		method: 'GET',
+		network: true,
+	};
+	const result = await loadTest(options)
+	await server.close()
+	console.log(server)
+	console.log(`Requests received: ${server.totalRequests}`)
+	result.show()
+}
+
+await runNetworkPerformanceTest()
+
