91a6321c959a757f2a727f25246efe32d29a7759
Alex Fernández <alexfernandeznpm@gmail.com>
2023-09-21 15:46:01 +0200

    Improved tcp performance script to start the server on multiple cores

--- bin/tcp-performance.js
+++ bin/tcp-performance.js
@@ -1,21 +1,56 @@
 import {loadTest, startServer} from '../index.js'
+const cluster = await import('cluster')
+import {getHalfCores} from '../lib/cluster.js'
 
-const port = 7359;
+const port = 7360;
 const serverOptions = {port}
 
+if (cluster.isPrimary) {
+	await runTcpPerformanceTest()
+} else {
+	await runServer()
+}
 
 async function runTcpPerformanceTest() {
-	const server = await startServer(serverOptions)
+	const workers = await startWorkers()
 	const options = {
 		url: `http://localhost:${port}`,
 		method: 'GET',
 		tcp: true,
+		concurrency: 10,
 	};
 	const result = await loadTest(options)
-	await server.close()
-	console.log(`Requests received: ${server.totalRequests}`)
 	result.show()
+	console.log(`Test finished; closing server on ${workers.length} cores`)
+	await stopServer(workers)
 }
 
-await runTcpPerformanceTest()
+function startWorkers() {
+	return new Promise(resolve => {
+		const cores = getHalfCores()
+		const workers = []
+		for (let i = 0; i < cores; i++) {
+			const worker = cluster.fork()
+			worker.on('message', async () => {
+				workers.push(worker)
+				if (workers.length != cores) {
+					return
+				}
+				console.log(`Server started on ${workers.length} cores`)
+				return resolve(workers)
+			})
+		}
+	})
+}
+
+function stopServer(workers) {
+	for (const worker of workers) {
+		worker.kill('SIGTERM')
+	}
+}
+
+async function runServer() {
+	await startServer(serverOptions)
+	process.send('server ready')
+}
 
